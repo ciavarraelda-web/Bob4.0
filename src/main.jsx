bb2d13f813ce5c35010179e25fcae30499be1ce2
ciavarraelda-web <ciavarraelda@gmail.com>
2025-08-29 00:43:56 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PokerGame from './PokerGame';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PokerGame />
+  </React.StrictMode>
+);
